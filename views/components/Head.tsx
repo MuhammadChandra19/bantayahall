94d164cece11d6b0d45d23e93280df34caf78d92
Muhammad chandra zulfikar <Chanjull19@gmail.com>
2020-07-15 17:38:43 +0700

    initial commit

---
+++ views/components/Head.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import NextHead from 'next/head'
+
+interface HeaderProps {
+  pageTitle: string
+  description: string
+  currentURL?: string
+  previewImage?: string
+  siteName?: string
+}
+
+const Head: React.FC<HeaderProps> = ({
+  pageTitle,
+  description,
+  currentURL = 'current',
+  previewImage = '',
+  siteName = '',
+}) => {
+  return (
+    <NextHead>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <meta name="description" content={description} />
+      <meta property="og:title" content={pageTitle} key="ogtitle" />
+      + <meta property="og:description" content={description} key="ogdesc" />
+      <meta property="og:url" content={currentURL} key="ogurl" />
+      <meta property="og:image" content={previewImage} key="ogimage" />
+      <meta property="og:site_name" content={siteName} key="ogsitename" />
+      <meta property="og:title" content={pageTitle} key="ogtitle" />
+      <meta property="og:description" content={description} key="ogdesc" />
+      <title>{pageTitle}</title>
+      <link rel="icon" href="/favicon.ico" />
+    </NextHead>
+  )
+}
+
+export default Head
